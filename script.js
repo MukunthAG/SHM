c649281c4f38df230714467b35cd803a66487a0a
Mukunth  A G <72994221+MukunthAG@users.noreply.github.com>
2021-01-20 12:56:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var elt = document.getElementById('calculator');
+var calculator = Desmos.GraphingCalculator(elt, {keypad: false, pasteGraphLink: true, zoomButtons: true});
+calculator.setExpression({ id: 'graph1'});
+
+
